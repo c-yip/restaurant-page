e26d7bcfcd1e03474f01ca03a4795cb51368a349
Christian <christianyip92@gmail.com>
2022-08-22 13:25:33 -0700

    Add function to create title for home page.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 console.log('Test to see if if everything is working!')
 import { createNavBar } from "./nav.js";
-import { appendHome } from "./home.js";
+import { createHome } from "./home.js";
 const body = document.querySelector('body');
 createNavBar();
+createHome();
