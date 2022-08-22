6f5057583646585b7235c999750494b366bd94c7
Christian <christianyip92@gmail.com>
2022-08-22 13:08:09 -0700

    Add nav bar creation function.

--- src/nav.js
+++ src/nav.js
@@ -21,6 +21,60 @@ export function createNavBar() {
     a1.appendChild(img1);
     const text = document.createTextNode(`Charlotte's Dumplings`)
     a1.appendChild(text);
+
+    const span1 = document.createElement('span');
+    span1.classList.add('navbar-text');
+    span1.textContent = `123 Fake St., Los Angeles, CA 90012`;
+    div1.appendChild(span1);
+
+    const span2 = document.createElement('span');
+    span2.classList.add('navbar-text');
+    span2.textContent = `(213) 321-5862`;
+    div1.appendChild(span2);
+
+    const ul = document.createElement('ul');
+    ul.classList.add('navbar-nav');
+    div1.appendChild(ul);
+
+    const li1 = document.createElement('li');
+    li1.classList.add('nav-item');
+    ul.appendChild(li1);
+
+    const a2 = document.createElement('a');
+    a2.classList.add('nav-link', 'active');
+    a2.setAttribute('href', '#')
+    li1.appendChild(a2);
+    a2.textContent = 'HOME';
+
+    const li2 = document.createElement('li');
+    li2.classList.add('nav-item');
+    ul.appendChild(li2);
+
+    const a3 = document.createElement('a');
+    a3.classList.add('nav-link', 'active');
+    a3.setAttribute('href', '#')
+    li2.appendChild(a3);
+    a3.textContent = 'MENU';
+
+    const li3 = document.createElement('li');
+    li3.classList.add('nav-item');
+    ul.appendChild(li3);
+
+    const a4 = document.createElement('a');
+    a4.classList.add('nav-link', 'active');
+    a4.setAttribute('href', '#')
+    li3.appendChild(a4);
+    a4.textContent = 'HOURS & LOCATION';
+
+    const li4 = document.createElement('li');
+    li4.classList.add('nav-item');
+    ul.appendChild(li4);
+
+    const a5 = document.createElement('a');
+    a5.classList.add('nav-link', 'active');
+    a5.setAttribute('href', '#');
+    li4.appendChild(a5);
+    a5.textContent = 'CONTACT US';
 }
 
 /* <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
